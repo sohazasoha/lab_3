bfd18ef474196c5ae85c1d61fd1514932ab57408
sohazasoha <sonyapeeva2003@mail.ru>
2020-12-23 20:01:44 +0300

    lab_3.fix

--- main.py
+++ main.py
@@ -1,70 +1,90 @@
 from struct import pack
-import numpy as np
+from math import sin, pi
 
 
-def dots_creating():
-    t = np.linspace(0, 2*np.pi, 300)
-    np_x = np.sin(3*t+np.pi/2)
-    np_y = np.sin(2*t)
-    x = [round(i, 2) for i in np_x]
-    y = [round(i, 2) for i in np_y]
-    x_min, y_min = min(x), min(y)
-    dots = [(x[i], y[i]) for i in range(len(x))]
-    dots.reverse()
-    return x_min, y_min, dots
+class ImageGenerator:
+    def __init__(self, file_name, width, height, x, y, start_t, stop_t, step_t):
+        self.file = file_name
+        self.width = width
+        self.height = height
+        self.func_x = x
+        self.func_y = y
+        self.min_x = float('inf')
+        self.min_y = float('inf')
+        self.start_t = start_t
+        self.stop_t = stop_t
+        self.step_t = step_t
+        self.round_t = len(str(step_t)) - 2
 
+    def __header_creating(self):
+        file_type = 19778
+        res_1 = 0
+        res_2 = 0
+        pix_data = 62
+        file_size = pix_data + self.width + self.height
+        return pack('<HL2HL', file_type, file_size, res_1, res_2, pix_data)
 
-def header_creating(width, height):
-    file_type = 19778
-    res_1 = 0
-    res_2 = 0
-    pix_data = 62
-    file_size = pix_data * width * height
-    return pack('<HL2HL', file_type, file_size, res_1, res_2, pix_data)
+    def __info_creating(self):
+        header_size = 40
+        planes = 1
+        pix_bits = 8
+        compression = 0
+        image_size = 0
+        x_pix_m = 0
+        y_pix_m = 0
+        total_colors = 2
+        important_colors = 0
+        return pack('<3L2H6L', header_size, self.width, self.height,
+                    planes, pix_bits, compression, image_size, x_pix_m, y_pix_m,
+                    total_colors, important_colors)
 
+    def __color_table_creating(self):
+        color_1 = (0, 0, 0, 0)
+        color_2 = (255, 255, 255, 0)
+        return pack('<8B', *color_1, *color_2)
 
-def info_creating(width, height):
-    header_size = 40
-    image_width = width
-    image_height = height
-    planes = 1
-    pix_bits = 8
-    compression = 0
-    image_size = 0
-    x_pix = 0
-    y_pix = 0
-    total_colors = 2
-    important_colors = 0
-    return pack('<3L2H6L', header_size,
-                image_width, image_height,
-                planes, pix_bits, compression,
-                image_size, x_pix, y_pix,
-                total_colors, important_colors)
+    def __dots_creating(self):
+        dots = []
+        t = self.start_t
+        while t <= self.stop_t:
+            x = round(self.func_x(t), self.round_t)
+            if x <= self.min_x:
+                self.min_x = x
+            y = round(self.func_y(t), self.round_t)
+            if y <= self.min_y:
+                self.min_y = y
+            dots.append((x, y))
+            t += self.step_t
+        dots.reverse()
+        return dots
 
-
-def color_table_creating():
-    color_1 = (0, 0, 0, 0)
-    color_2 = (255, 255, 255, 0)
-    return pack('<8B', *color_1, *color_2)
-
-
-def file_writing(step, width, height, file_name):
-    with open(f'{file_name}.bmp', 'wb') as f:
-        f.write(header_creating(width, height))
-        f.write(info_creating(width, height))
-        f.write(color_table_creating())
-        x_min, y_min, dots = dots_creating()
-        pix_y = y_min
-        for i in range(height):
-            pix_x = x_min
-            for j in range(width):
-                if (pix_x, pix_y) in dots:
-                    f.write(pack('<B', 0))
-                else:
-                    f.write(pack('<B', 1))
-                pix_x = round(pix_x + step, 2)
-            pix_y = round(pix_y + step, 2)
+    def file_writing(self):
+        with open(f'{self.file}.bmp', 'wb') as f:
+            f.write(self.__header_creating())
+            f.write(self.__info_creating())
+            f.write(self.__color_table_creating())
+            dots = self.__dots_creating()
+            pix_y = self.min_y
+            for cyc_y in range(self.height):
+                pix_x = self.min_x
+                for cyc_x in range(self.width):
+                    if (pix_x, pix_y) in dots:
+                        f.write(pack('<B', 0))
+                    else:
+                        f.write(pack('<B', 1))
+                    pix_x = round(pix_x + self.step_t, self.round_t)
+                pix_y = round(pix_y + self.step_t, self.round_t)
 
 
 if __name__ == '__main__':
-    file_writing(0.01, 300, 300, 'koala')
+    image = ImageGenerator(
+        file_name='koala',
+        width=300,
+        height=300,
+        start_t=0,
+        stop_t=2 * pi,
+        step_t=0.01,
+        x=lambda t: sin(3*t + pi/2),
+        y=lambda t: sin(2*t)
+    )
+    image.file_writing()
